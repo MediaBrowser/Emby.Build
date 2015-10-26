61912d6ffc9d9d9af5b5855746658b0b24fc286f
Carlos Hernandez <carlos@techbyte.ca>
2015-10-26 16:31:42 -0600

    Added wily build.

--- debfiles/restart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/bin/sudo service emby-server restart
--- obs/prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-update_release() {
-	sed -i -e 's:emby-server-beta\ source:emby-server\ source:' source.lintian-overrides
-}

569ee697f4e9114465eca9a695c0967cbb701bcf
Carlos Hernandez <carlos@techbyte.ca>
2015-11-30 23:34:09 -0700

    Need to further edit sudoers file and restart.

--- emby-builder/debfiles/restart.sh
+++ emby-builder/debfiles/restart.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-/usr/bin/sudo /bin/systemctl restart emby-server.service
+NAME=emby-server
+
+if which systemctl > /dev/null 2>&1; then
+  sudo /bin/systemctl restart ${NAME}.service
+elif which service >/dev/null 2>&1; then
+  sudo /usr/sbin/service ${NAME} restart
+elif which invoke-rc.d >/dev/null 2>&1; then
+  invoke-rc.d ${NAME} restart
+else
+  sudo /etc/init.d/${NAME} restart
+fi

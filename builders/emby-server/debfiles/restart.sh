0261d9f77f29ed4c98d625ed3113a957c0cdb4a7
Carlos Hernandez <carlos@techbyte.ca>
2015-12-11 14:35:28 -0700

    Testing kiwi image build.

--- builders/emby-server/debfiles/restart.sh
+++ builders/emby-server/debfiles/restart.sh
@@ -1,13 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 NAME=emby-server
 
-if which systemctl > /dev/null 2>&1; then
-  sudo /bin/systemctl restart ${NAME}.service
-elif which service >/dev/null 2>&1; then
-  sudo /usr/sbin/service ${NAME} restart
-elif which invoke-rc.d >/dev/null 2>&1; then
-  sudo /usr/sbin/invoke-rc.d ${NAME} restart
-else
-  sudo /etc/init.d/${NAME} restart
+restart_cmds=("/bin/s6-svc -t /var/run/s6/services/${NAME}" \
+  "systemctl restart ${NAME}.service" \
+  "service ${NAME} restart" \
+  "/etc/init.d/${NAME} restart" \
+  "invoke-rc.d ${NAME} restart")
+
+PIDFILE=`find /var/run -name "emby*.pid" -print -quit`
+[ -n "$PIDFILE" ] && EMBY_PID=`cat ${PIDFILE} 2> /dev/null || true`
+
+for restart_cmd in "${restart_cmds[@]}"; do
+  exec /usr/bin/sudo $restart_cmd > /dev/null 2>&1 || true
+  sleep 1
+
+  if ! kill -0 $EMBY_PID > /dev/null; then
+    break
+  fi
+done
+
+# is emby still running? Might have been manually started
+if kill -0 $EMBY_PID > /dev/null 2>&1; then
+  CPIDS=$(pgrep -P $EMBY_PID)
+  sleep 2 && kill -KILL $CPIDS
+  kill -TERM $CPIDS > /dev/null 2>&1
+  # restart it
+  exec emby-server start &
 fi

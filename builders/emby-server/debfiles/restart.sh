e9e3566e9d1d2f36315144b72dd182a5154c15d3
Carlos Hernandez <carlos@techbyte.ca>
2016-01-11 18:41:10 -0700

    Fixing restart script.

--- builders/emby-server/debfiles/restart.sh
+++ builders/emby-server/debfiles/restart.sh
@@ -2,8 +2,8 @@
 
 NAME=emby-server
 
-restart_cmds=("/bin/s6-svc -t /var/run/s6/services/${NAME}" \
-  "systemctl restart ${NAME}.service" \
+restart_cmds=("s6-svc -t /var/run/s6/services/${NAME}" \
+  "systemctl restart ${NAME}" \
   "service ${NAME} restart" \
   "/etc/init.d/${NAME} restart" \
   "invoke-rc.d ${NAME} restart")
@@ -12,19 +12,9 @@ PIDFILE=`find /var/run -name "emby*.pid" -print -quit`
 [ -n "$PIDFILE" ] && EMBY_PID=`cat ${PIDFILE} 2> /dev/null || true`
 
 for restart_cmd in "${restart_cmds[@]}"; do
-  exec /usr/bin/sudo $restart_cmd > /dev/null 2>&1 || true
-  sleep 1
-
-  if ! kill -0 $EMBY_PID > /dev/null; then
-    break
+  cmd=$(echo "$restart_cmd" | awk '{print $1}')
+  cmd_loc=$(command -v ${cmd})
+  if [ -n $cmd_loc ]; then
+    exec sudo $restart_cmd > /dev/null 2>&1 || true
   fi
 done
-
-# is emby still running? Might have been manually started
-if kill -0 $EMBY_PID > /dev/null 2>&1; then
-  CPIDS=$(pgrep -P $EMBY_PID)
-  sleep 2 && kill -KILL $CPIDS
-  kill -TERM $CPIDS > /dev/null 2>&1
-  # restart it
-  exec emby-server start &
-fi

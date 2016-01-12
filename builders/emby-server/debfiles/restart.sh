305698b61b61b0df873384cd0848b1a8ff1ea6a6
Carlos Hernandez <carlos@techbyte.ca>
2016-01-12 16:22:47 -0700

    Fixing sudoers file and updating restart script.     * script will now work with various locations of service + systemctl

--- builders/emby-server/debfiles/restart.sh
+++ builders/emby-server/debfiles/restart.sh
@@ -15,6 +15,7 @@ for restart_cmd in "${restart_cmds[@]}"; do
   cmd=$(echo "$restart_cmd" | awk '{print $1}')
   cmd_loc=$(command -v ${cmd})
   if [ -n $cmd_loc ]; then
+    restart_cmd=$(echo $restart_cmd | sed -e "s%${cmd}%${cmd_loc}%")
     exec sudo $restart_cmd > /dev/null 2>&1 || true
     sleep 1
     if ! kill -0 $EMBY_PID > /dev/null; then

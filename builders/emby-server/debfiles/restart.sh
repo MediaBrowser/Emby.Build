ecdcc0e9e245e0e52d59ce955978804e10b146dd
Carlos Hernandez <carlos@techbyte.ca>
2016-09-08 21:25:03 -0600

    Changing maintianer scripts to bash. Fixing restart. Adding mono-devel as req.

--- builders/emby-server/debfiles/restart.sh
+++ builders/emby-server/debfiles/restart.sh
@@ -5,21 +5,14 @@ NAME=emby-server
 restart_cmds=("s6-svc -t /var/run/s6/services/${NAME}" \
   "systemctl restart ${NAME}" \
   "service ${NAME} restart" \
-  "/etc/init.d/${NAME} restart" \
-  "invoke-rc.d ${NAME} restart")
-
-PIDFILE=`find /var/run -name "emby*.pid" -print -quit`
-[ -n "$PIDFILE" ] && EMBY_PID=`cat ${PIDFILE} 2> /dev/null || true`
+  "/etc/init.d/${NAME} restart") 
 
 for restart_cmd in "${restart_cmds[@]}"; do
   cmd=$(echo "$restart_cmd" | awk '{print $1}')
   cmd_loc=$(command -v ${cmd})
-  if [ -n "$cmd_loc" ]; then
+  if [[ -n "$cmd_loc" ]]; then
     restart_cmd=$(echo "$restart_cmd" | sed -e "s%${cmd}%${cmd_loc}%")
-    exec sudo $restart_cmd > /dev/null 2>&1 || true
-    sleep 1
-    if ! kill -0 $EMBY_PID > /dev/null; then
-      break
-    fi
+    echo "sleep 2; sudo $restart_cmd > /dev/null 2>&1" | at now > /dev/null 2>&1
+    exit 0
   fi
 done
